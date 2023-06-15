8349dfe840fc8d6dfb8960bdc9900b4190db16a2
Yarikril <koshmanov07@mail.ru>
2023-06-15 20:24:41 +0300

    initial

---
+++ Kurs1.py
@@ -0,0 +1,26 @@
+'''def str_counter(s):   # O (N*M) S = 30
+    for sym in set(s):
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count+=1
+        print(sym, count)
+
+str_counter('abcnra')
+
+def str_counter(s):   # O (N**2) S = 36
+    for sym in s:
+        count = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                count+=1
+        print(sym, count)
+
+str_counter('abcnra')'''
+def str_counter(s): #O (N)
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    for syms, count in syms_counter.items():
+        print(syms, count)
+str_counter('abcnra')
